0076361e384a83157f2ab91ddd51da67b96ca880
htgentry14 <82285186+htgentry14@users.noreply.github.com>
2022-09-02 16:03:50 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp6activity7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.enterAmount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(347, 184);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 0;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(322, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // enterAmount
+            // 
+            this.enterAmount.AutoSize = true;
+            this.enterAmount.Location = new System.Drawing.Point(203, 104);
+            this.enterAmount.Name = "enterAmount";
+            this.enterAmount.Size = new System.Drawing.Size(86, 16);
+            this.enterAmount.TabIndex = 2;
+            this.enterAmount.Text = "Enter Amount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.enterAmount);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.calculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label enterAmount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6activity7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            double approx3(int terms)
+            {
+                double sum = 0.0;
+                int i = 1, x = 1, po = 0;
+                while (i <= terms)
+                {
+                    if (i % 2 != 0)
+                        sum = sum + (double)sqrt(12) / (x * pow(3, po));
+                    else
+                        sum = sum - (double)sqrt(12) / (x * pow(3, po));
+                    po++;
+                    x += 2;
+                    i++;
+                }
+                return sum;
+            }
+            double approx2(int terms)
+            {
+                double sum = 0.0;
+                int i = 1, x = 1;
+                while (i <= terms)
+                {
+                    if (i % 2 != 0)
+                        sum = sum + (double)12 / (x * x);
+                    else
+                        sum = sum - (double)12 / (x * x);
+                    x++;
+                    i++;
+                }
+                return sqrt(sum);
+            }
+            double approx1(int terms)
+            {
+                double sum = 0.0;
+                int i = 1, x = 1;
+                while (i <= terms)
+                {
+                    if (i % 2 != 0)
+                        sum = sum + (double)4 / x;
+                    else
+                        sum = sum - (double)4 / x;
+                    x += 2;
+                    i++;
+                }
+                return sum;
+            }
+            int main(void)
+            {
+                int terms;
+                do
+                {
+                    printf("\n\nEnter the number of terms to approximate pi, or 0 to terminate: ");
+                    Scale("%d", &terms);
+                    if (terms < 0)
+                    {
+                        printf("\n\tWrong input, please try again..");
+                        continue;
+                    }
+                    else if (terms == 0)
+                    {
+                        printf("\n\t***** Program Terminated *****");
+                        break;
+                    }
+                    else
+                    {
+                        printf("\nFirst approximation of pi to 10000 terms is : %.8lf", approx1(terms));
+                        printf("\nSecond approximation of pi to 10000 terms is : %.8lf", approx2(terms));
+                        printf("\nThird approximation of pi to 10000 terms is : %.8lf", approx3(terms));
+                    }
+                } while (terms != 0);
+                printf("\n");
+                return 0;
+            }
+#pragma warning restore CS8321 // Local function is declared but never used
+        }
+
+        private void printf(string v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private unsafe void Scale(string v1, int* v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void printf(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double sqrt(double sum)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int pow(int v, int po)
+        {
+            throw new NotImplementedException();
+        }
+
+        private double sqrt(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            OnClick(null);
+        }
+    }
+}
